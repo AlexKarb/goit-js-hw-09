d11144316895ed1577df202c84c735ee8586dc9d
AlexKarb <alex.workinfo97@gmail.com>
2021-11-11 11:58:14 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+import changeColor from "./change-color";
+
+
+
+const startChangeColorBtn = document.querySelector('[data-start]');
+const stopChangeColorBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+
+startChangeColorBtn.addEventListener('click', () => {
+
+    timerId = setInterval(() => {
+        document.body.style.background = String(changeColor());
+    }, 1000);
+    
+    startChangeColorBtn.setAttribute('disabled', 'disabled');
+
+});
+
+
+
+stopChangeColorBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startChangeColorBtn.removeAttribute('disabled');
+})
+
 
